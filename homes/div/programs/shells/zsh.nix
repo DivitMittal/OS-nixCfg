f2c49d8a757be95ae8517d01f1b44665521be012
Divit Mittal <64.69.76.69.74.m@gmail.com>
2024-04-16 14:49:33 +0530

    added zsh

--- homes/div/programs/shells/zsh.nix
+++ homes/div/programs/shells/zsh.nix
@@ -3,13 +3,44 @@ _:
 {
   programs.zsh = {
     enable = true;
+
     dotDir = ".config/zsh";
     history = {
       extended              = false; # save timestamps as well
       expireDuplicatesFirst = true; ignoreAllDups = true;
       path                  = "$ZDOTDIR/.zsh_history";
+      share = true;
     };
     defaultKeymap = null;
+
+    profileExtra = ''
+      # Place for hosting Git repos
+      export GIT_HOSTING='git@github.com:DivitMittal'
+
+      # Don't check mail when opening terminal.
+      unset MAILCHECK
+
+      # Change this to your console based IRC client of choice.
+      export IRC_CLIENT='weechat'
+
+      # Set Xterm/screen/Tmux title with only a short hostname.
+      export SHORT_HOSTNAME=$(hostname -s)
+
+      # Set Xterm/screen/Tmux title with only a short username.
+      export SHORT_USER=''${USER:0:8}
+
+      # Set Xterm/screen/Tmux title with shortened command and directory.
+      export SHORT_TERM_LINE=true
+    '';
+
+    enableCompletion = true;
+    syntaxHighlighting = { enable = true; };
+    autosuggestion = {
+      enable    = true;
+      highlight = "fg = #ff00ff,bg = cyan,bold,underline";
+    };
+
+    autocd = true;
 
     zsh-abbr = {
       enable = true;
@@ -25,19 +56,11 @@ _:
       };
     };
 
-    autosuggestion = {
-      enable    = true;
-      highlight = "fg = #ff00ff,bg = cyan,bold,underline";
-    };
-
-    autocd        = true;
-
     antidote = {
       enable  = true;
       plugins = [
-        "jeffreytse/zsh-vi-mode"
         "ohmyzsh/ohmyzsh" "ohmyzsh/ohmyzsh path:plugins/macos" "ohmyzsh/ohmyzsh path:plugins/git"
-        "hlissner/zsh-autopair"
+        "hlissner/zsh-autopair" "jeffreytse/zsh-vi-mode" "Aloxaf/fzf-tab"
       ];
     };
   };
