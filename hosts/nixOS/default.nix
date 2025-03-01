e823801017a0d442030e08e1a00e7499d89e4185
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-03-01 15:11:38 +0530

    Added: droid & nixOS(WSL & L2) flake modules

--- hosts/nixOS/default.nix
+++ hosts/nixOS/default.nix
@@ -1,6 +1,45 @@
-{ pkgs, ... }:
+{ self, inputs, ... }:
 
 {
-  flake.nixosConfigurations = pkgs.lib.nixosSystem {
+  flake.nixosConfigurations = {
+    L2 = let
+      hostname = "L2";
+      system = "x86_64-linux";
+    in inputs.nixpkgs.lib.nixosSystem {
+        inherit system;
+        pkgs = import inputs.nixpkgs { inherit system; };
+        specialArgs = {
+          user = self.user;
+          inherit hostname;
+          # pkgs-stable = import inputs.nixpkgs-stable { inherit system; };
+        };
+
+        modules = [
+          ./../common
+          ./common
+          ./${hostname}
+          inputs.nix-index-database.darwinModules.nix-index { programs.nix-index.enable = false; programs.nix-index-database.comma.enable = true; }
+        ];
+    };
+
+    WSL = let
+      hostname = "WSL";
+      system = "x86_64-linux";
+    in inputs.nixpkgs.lib.nixosSystem {
+        inherit system;
+        pkgs = import inputs.nixpkgs { inherit system; };
+        specialArgs = {
+          user = self.user;
+          inherit hostname;
+          # pkgs-stable = import inputs.nixpkgs-stable { inherit system; };
+        };
+
+        modules = [
+          ./../common
+          ./common
+          inputs.nix-index-database.darwinModules.nix-index { programs.nix-index.enable = false; programs.nix-index-database.comma.enable = true; }
+          inputs.nixos-wsl.nixosModules.default
+        ];
+    };
   };
 }
