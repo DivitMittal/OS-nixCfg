c635bf3ccb63c52ef224f9a9144ed90e8daa7159
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-03-10 18:13:47 +0530

    Refactoring: moving networking to common

--- hosts/nixOS/L2/default.nix
+++ hosts/nixOS/L2/default.nix
@@ -1,10 +1,5 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 
 {
   fonts.packages = with pkgs; [ nerd-fonts.caskaydia-cove ];
-
-  networking = {
-    computerName = "${config.networking.hostname}";
-    # hostName = "${hostname}"; # handled by easy-hosts
-  };
 }
