1dc6436f960c062cbbbb4ae5ef3be36e74dab45e
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-03-10 18:13:47 +0530

    Refactoring: moving networking to common

--- hosts/nixOS/common/default.nix
+++ hosts/nixOS/common/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   programs.man.enable  = true;
@@ -11,6 +11,9 @@
             "1.1.1.1"              "1.0.0.1"         # IPv4
       "2606:4700:4700::1111" "2606:4700:4700::1001"  # IPv6
     ];
+
+    # hostName = "${hostname}"; # handled by easy-hosts
+    computerName = "${config.networking.hostName}";
   };
 
   environment.systemPackages = builtins.attrValues {
