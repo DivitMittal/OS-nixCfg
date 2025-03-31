9de10bc8f30d9bd933006c1e42af688c72584911
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-04-01 03:57:36 +0530

    feat: added symlink to default storage home on Android to nix-on-droid home directory

--- hosts/droid/M1/home/default.nix
+++ hosts/droid/M1/home/default.nix
@@ -1,8 +1,10 @@
-{ self, ... }:
+{ self, config, ... }:
 
 {
   imports = [
     (self + /home/common)
     (self + /home/tty-env)
   ];
+
+  home.file."storage" = config.lib.file.mkOutOfStoreSymlink /storage/emulated/0; # impure
 }
