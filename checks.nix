70ecdd192b0a09925b682f456919cbcd8bcdb78a
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-04-01 03:19:49 +0530

    feat: added git pre-commit hooks the nix way including various checks

---
+++ checks.nix
@@ -0,0 +1,51 @@
+{ inputs, ... }:
+
+{
+  imports = [
+    inputs.pre-commit-hooks.flakeModule
+  ];
+
+  perSystem.pre-commit = {
+    check.enable = true;
+
+    settings = {
+      src = ./.;
+      excludes = [ "flake.lock" ];
+      default_stages = [ "pre-commit" ];
+      hooks = {
+        check-added-large-files = {
+          enable = true;
+          excludes = [
+            "\\.png"
+            "\\.jpg"
+          ];
+        };
+
+        check-case-conflicts.enable = true;
+        check-executables-have-shebangs.enable = true;
+        check-shebang-scripts-are-executable.enable = false; # many of the scripts in the config aren't executable because they don't need to be.
+        check-merge-conflicts.enable = true;
+        detect-private-keys.enable = true;
+        fix-byte-order-marker.enable = true;
+        mixed-line-endings.enable = false;
+        trim-trailing-whitespace.enable = true;
+
+        # ========== nix ==========
+        nixfmt-rfc-style.enable = true;
+        deadnix = {
+          enable = true;
+          settings = {
+            noLambdaArg = true;
+          };
+        };
+        nil.enable = true;
+        statix.enable = true;
+
+        # ========== shellscripts ==========
+        shfmt.enable = true;
+        shellcheck.enable = true;
+        end-of-file-fixer.enable = true;
+      };
+    };
+  };
+}
