7bf9e6d702ab597aa787b9b4266d42bc64b52369
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-04-03 01:02:16 +0530

    refactor: fetching oauth2.py from github instead

---
+++ home/comms/email/oauth.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+let
+  mutt = pkgs.fetchFromGitHub {
+    owner = "muttmua";
+    repo = "mutt";
+    rev = "master";
+    sha256 = "sha256-y9tXDwG0nC1HLKDq0B6DONsM0/hkGjsylwjlVhHEutY=";
+  };
+in
+{
+  home.file."${config.home.sessionVariables.BIN_HOME}/oauth2".source = (mutt + /contrib/mutt_oauth2.py);
+}
--- home/comms/email/oauth/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, ... }:
-
-{
-  home.file.oauth2 = {
-    enable = true;
-    source = ./mutt_oauth2.py;
-    target = "${config.home.sessionVariables.BIN_HOME}/oauth2";
-  };
-}
