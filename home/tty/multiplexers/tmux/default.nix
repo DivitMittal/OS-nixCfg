870cddadade095e2a408fd8a98ecea26e8710578
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-04-03 18:51:18 +0530

    fixup! migrated: local tmux to oh-my-tmux fetchFromGitHub configuration

--- home/tty/multiplexers/tmux/default.nix
+++ home/tty/multiplexers/tmux/default.nix
@@ -1,20 +1,20 @@
 {pkgs, ...}: let
-  oh-my-tmux = pkgs.fetchFromGitHub {
-    owner = "gpakosz";
-    repo = ".tmux";
-    rev = "master";
-    sha256 = "sha256-2mdbOKCiwkr3FgjjyThezL+VVf5nm3+04idMETBLeao=";
-  };
+  # oh-my-tmux = pkgs.fetchFromGitHub {
+  #   owner = "gpakosz";
+  #   repo = ".tmux";
+  #   rev = "master";
+  #   sha256 = "sha256-2mdbOKCiwkr3FgjjyThezL+VVf5nm3+04idMETBLeao=";
+  # };
 in
 {
   # programs.tmux = {
   #   enable = true;
   #   package = pkgs.tmux;
   # };
-  xdg.configFile."tmux/tmux.conf" = {
-    source = oh-my-tmux + /.tmux.conf;
-  };
-  xdg.configFile."tmux/tmux.conf.local" = {
-    source = ./tmux.conf.local;
-  };
+  # xdg.configFile."tmux/tmux.conf" = {
+  #   source = oh-my-tmux + /.tmux.conf;
+  # };
+  # xdg.configFile."tmux/tmux.conf.local" = {
+  #   source = ./tmux.conf.local;
+  # };
 }
