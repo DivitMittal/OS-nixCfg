74f222292df503f83dc62705efddca0a1e916c25
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-04-24 07:52:19 +0530

    feat: support for nix-your-shell, nix-init, nurl, nix-index

---
+++ home/common/helpers.nix
@@ -0,0 +1,39 @@
+{inputs, pkgs, config, ...}: {
+  imports = [
+    inputs.nix-index-database.hmModules.nix-index
+  ];
+  programs.nix-index-database.comma.enable = true;
+  programs.nix-index = {
+    enable = true;
+    package = pkgs.nix-index;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+  };
+
+  programs.nix-your-shell = {
+    enable = true;
+    package = pkgs.nix-your-shell;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+    enableNushellIntegration = false;
+  };
+
+  programs.nix-init = {
+    enable = true;
+    package = pkgs.nix-init;
+    settings = {
+      maintainers = [
+        config.hostSpec.handle
+      ];
+      commit = true;
+      nixpkgs = "<nixpkgs>";
+    };
+  };
+
+  home.packages = builtins.attrValues {
+    inherit(pkgs)
+      nurl
+    ;
+  };
+}
