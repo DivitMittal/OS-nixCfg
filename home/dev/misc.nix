a43ff7f9c9f54ab4357ec0a76830fd5c1489f069
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-04-29 05:33:02 +0530

    updates: minor & gitignore

--- home/dev/misc.nix
+++ home/dev/misc.nix
@@ -19,16 +19,12 @@
       lua
       # Rust
       cargo
-      ## IDE
-      #vscode
-      #rstudio
       ;
 
     vscode =
       if hostPlatform.isDarwin
       then pkgs.brewCasks.visual-studio-code
       else pkgs.vscode;
-    #luarocks = pkgs.luajitPackages.luarocks;
   };
 
   home.sessionPath = lib.mkAfter ["${config.home.homeDirectory}/.cargo/bin"];
