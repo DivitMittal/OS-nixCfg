3611042d3a4c12cc4fbf2278386a0b4ebce047d0
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-04 21:28:50 +0530

    bump: source mutt hash

--- home/comms/email/oauth.nix
+++ home/comms/email/oauth.nix
@@ -6,8 +6,8 @@
   mutt = pkgs.fetchFromGitHub {
     owner = "muttmua";
     repo = "mutt";
-    rev = "master";
-    sha256 = "sha256-y9tXDwG0nC1HLKDq0B6DONsM0/hkGjsylwjlVhHEutY=";
+    rev = "5fd040e3aa1f807db1cab4ca7d7ba5fc8d48722e";
+    hash = "sha256-DyFCczkCQCV9egIY/gWNLGtcxlFEWpprBHGdIS8+OFU=";
   };
 in {
   home.file."${config.home.sessionVariables.BIN_HOME}/oauth2".source = mutt + /contrib/mutt_oauth2.py;
