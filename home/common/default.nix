ba60f6f85caadaa17e1fcd330ad4d2bbf14dc846
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-10 23:19:07 +0530

    refactor: decouple home-manager settings configuration

--- home/common/default.nix
+++ home/common/default.nix
@@ -1,37 +1,3 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
+{lib, ...}: {
   imports = lib.custom.scanPaths ./.;
-
-  home = {
-    inherit (config.hostSpec) username;
-    homeDirectory = config.hostSpec.home;
-  };
-
-  programs.home-manager.enable = true; # home-manager standalone
-  news.display = "show";
-
-  xdg.enable = true;
-  home.preferXdgDirectories = true;
-  home.enableNixpkgsReleaseCheck = true;
-
-  home.packages = builtins.attrValues {
-    my-hello = pkgs.writeShellScriptBin "my-hello" ''echo "Hello, ${config.home.username}!"'';
-  };
-  home.extraOutputsToInstall = ["info"]; # "doc" "devdoc"
-
-  home.language = {
-    base = "en_US.UTF-8";
-  };
-
-  manual = {
-    html.enable = true;
-    json.enable = false;
-    manpages.enable = false;
-  };
-
-  home.stateVersion = "25.05";
 }
---
+++ home/common/manager.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home = {
+    inherit (config.hostSpec) username;
+    homeDirectory = config.hostSpec.home;
+  };
+
+  programs.home-manager.enable = true; # home-manager standalone
+  news.display = "show";
+
+  xdg.enable = true;
+  home.preferXdgDirectories = true;
+  home.enableNixpkgsReleaseCheck = true;
+
+  home.packages = builtins.attrValues {
+    my-hello = pkgs.writeShellScriptBin "my-hello" ''echo "Hello, ${config.home.username}!"'';
+  };
+  home.extraOutputsToInstall = ["info"]; # "doc" "devdoc"
+
+  home.language = {
+    base = "en_US.UTF-8";
+  };
+
+  manual = {
+    html.enable = true;
+    json.enable = false;
+    manpages.enable = false;
+  };
+
+  home.stateVersion = "25.05";
+}
