8a91ca5af84ae9a1422f4426f1af67acc2ed26d2
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-17 03:09:54 +0530

    refactor: seperating misc

---
+++ home/tools/privacy/misc.nix
@@ -0,0 +1,21 @@
+{pkgs, lib, config, ...}: {
+  home.packages = lib.attrsets.attrValues {
+    inherit
+      (pkgs)
+      age
+      #skate
+      ;
+  };
+
+  programs.gpg = {
+    enable = true;
+    package = pkgs.gnupg;
+
+    homedir = "${config.xdg.dataHome}/gnupg";
+
+    settings = {
+      no-comments = false;
+      s2k-cipher-algo = "AES128";
+    };
+  };
+}
