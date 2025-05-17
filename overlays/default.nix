57db37824a5411af827b1f26bf91e9d4537fd801
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-17 11:24:33 +0530

    feat: add nixpkgs overlays with flake outputs & fix rel paths

---
+++ overlays/default.nix
@@ -0,0 +1,25 @@
+{inputs, ...}: let
+  additions = final: prev: (
+    prev.lib.packagesFromDirectoryRecursive {
+      callPackage = prev.lib.customisation.callPackageWith final;
+      directory = ../pkgs;
+    }
+  );
+
+  master-pkgs = final: prev: {
+    master = import inputs.pkgs-master {
+      config = {
+        allowUnfree = true;
+        allowBroken = false;
+        allowUnsupportedSystem = false;
+        allowInsecure = true;
+      };
+    };
+  };
+in {
+  default = final: prev:
+    prev.lib.attrsets.mergeAttrsList [
+      (additions final prev)
+      (master-pkgs final prev)
+    ];
+}
