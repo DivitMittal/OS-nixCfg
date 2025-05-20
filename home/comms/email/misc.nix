b72fcbf949f79e9ab2e130cb67762fe29941fc36
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-20 23:56:19 +0530

    fix: cleanup extraneous `Resources` directory found in microsoft suite pkgs

--- home/comms/email/misc.nix
+++ home/comms/email/misc.nix
@@ -3,11 +3,29 @@
   hostPlatform,
   lib,
   ...
-}: {
+}: let
+  outlook = pkgs.brewCasks.microsoft-outlook.overrideAttrs (oldAttrs: {
+    installPhase =
+      oldAttrs.installPhase
+      + ''
+        # Clean Resources
+        echo "Removing entire Resources directory from microsoft-outlook installation..."
+        local resource_dir="$out/Resources"
+
+        if [ -d "$resource_dir" ]; then
+          echo "Found directory '$resource_dir' and removing it..."
+          rm -rf "$resource_dir"
+        else
+          echo "Warning: Resources directory '$resource_dir' not found. It may have been removed already or path is wrong." >&2
+        fi
+        echo "Finished cleanup (removed Resources) for microsoft-outlook."
+      '';
+  });
+in {
   home.packages = lib.attrsets.attrValues {
     outlook =
       if hostPlatform.isDarwin
-      then pkgs.brewCasks.microsoft-outlook
+      then outlook
       else null;
   };
 }
--- home/tools/notes.nix
+++ home/tools/notes.nix
@@ -3,7 +3,25 @@
   hostPlatform,
   lib,
   ...
-}: {
+}: let
+  onenote = pkgs.brewCasks.microsoft-onenote.overrideAttrs (oldAttrs: {
+    installPhase =
+      oldAttrs.installPhase
+      + ''
+        # Clean Resources
+        echo "Removing entire Resources directory from microsoft-onenote installation..."
+        local resource_dir="$out/Resources"
+
+        if [ -d "$resource_dir" ]; then
+          echo "Found directory '$resource_dir' and removing it..."
+          rm -rf "$resource_dir"
+        else
+          echo "Warning: Resources directory '$resource_dir' not found. It may have been removed already or path is wrong." >&2
+        fi
+        echo "Finished cleanup (removed Resources) for microsoft-onenote."
+      '';
+  });
+in {
   home.packages = lib.attrsets.attrValues {
     obsidian =
       if hostPlatform.isDarwin
@@ -11,7 +29,7 @@
       else pkgs.obsidian;
     onenote =
       if hostPlatform.isDarwin
-      then pkgs.brewCasks.microsoft-onenote
+      then onenote
       else null;
   };
 }
