a986c266d59d74809e4492fb0a43537a2107e018
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-21 20:07:58 +0530

    feat: added default outputs for modules

--- modules/flakeModule.nix
+++ modules/flakeModule.nix
@@ -1,17 +1,19 @@
-_: {
+{self, ...}: {
   flake.homeManagerModules = {
-    all = (builtins.import ./home);
+    all = builtins.import ./home;
+    default = self.outputs.homeManagerModules.all;
 
-    ai-chat = (builtins.import ./home/aichat.nix);
-    glow = (builtins.import ./home/glow.nix);
-    spicetify-cli = (builtins.import ./home/spicetify-cli.nix);
+    ai-chat = builtins.import ./home/aichat.nix;
+    glow = builtins.import ./home/glow.nix;
+    spicetify-cli = builtins.import ./home/spicetify-cli.nix;
   };
 
   flake.darwinModules = {
-    all = (builtins.import ./hosts/darwin);
+    all = builtins.import ./hosts/darwin;
+    default = self.outputs.darwinModules.all;
 
-    kanata = (builtins.import ./hosts/darwin/kanata.nix);
-    kanata-tray = (builtins.import ./hosts/darwin/kanata-tray.nix);
-    spotifyd = (builtins.import ./hosts/darwin/spotifyd.nix);
+    kanata = builtins.import ./hosts/darwin/kanata.nix;
+    kanata-tray = builtins.import ./hosts/darwin/kanata-tray.nix;
+    spotifyd = builtins.import ./hosts/darwin/spotifyd.nix;
   };
 }
