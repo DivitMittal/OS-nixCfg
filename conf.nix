13296952bfc1f6fd696dac9fbfdd0719e33a7a45
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-24 22:05:13 +0530

    feat: migrating to lix(a CppNix fork)

--- conf.nix
+++ conf.nix
@@ -7,7 +7,7 @@
 }: let
   inherit (lib) mkDefault;
 in {
-  nix.package = mkDefault pkgs.nixVersions.latest;
+  nix.package = mkDefault pkgs.lix;
 
   nix = {
     enable = true;
@@ -28,11 +28,13 @@ in {
         "https://divitmittal.cachix.org"
         "https://nix-community.cachix.org"
         "https://numtide.cachix.org"
+        "https://cache.lix.systems"
       ];
       trusted-public-keys = [
         "divitmittal.cachix.org-1:Fx7nQrvET1RKTTrxQHMDP/Relbu072af/MFG4BYvpjw="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
       ];
 
       trusted-users = ["${config.hostSpec.username}"];
