f9daae1faa25ceb9491f8b88004e9700109796af
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-25 20:20:26 +0530

    feat(darwin): bump stateVersion to 6 & removed non-flake features

--- hosts/darwin/common/default.nix
+++ hosts/darwin/common/default.nix
@@ -2,26 +2,42 @@
   config,
   lib,
   self,
-  inputs,
   pkgs,
   ...
 }: {
   imports = lib.custom.scanPaths ./.;
 
-  environment.darwinConfig = self + "/hosts/darwin/${config.networking.hostName}/default.nix";
-  system.primaryUser = "${config.hostSpec.username}";
-
   networking = {
     knownNetworkServices = ["Wi-Fi"];
     dns = [
-      # Cloudflare DNS
+      ## Cloudflare DNS
+      # IPv4
       "1.1.1.1"
-      "1.0.0.1" # IPv4
+      "1.0.0.1"
+      # IPv6
       "2606:4700:4700::1111"
-      "2606:4700:4700::1001" # IPv6
+      "2606:4700:4700::1001"
     ];
     computerName = "${config.networking.hostName}";
   };
+
+  programs.man.enable = true;
+  programs.info.enable = true;
+
+  system.stateVersion = 6;
+  #environment.darwinConfig = self + "/hosts/darwin/${config.networking.hostName}/default.nix"; # non-flake feature
+  system.primaryUser = "${config.hostSpec.username}";
+  system.tools = {
+    darwin-option.enable = true;
+    darwin-rebuild.enable = true;
+    darwin-uninstaller.enable = true;
+    darwin-version.enable = true;
+  };
+  system.checks = {
+    verifyBuildUsers = true;
+    verifyNixPath = false;
+  };
+  system.configurationRevision = self.rev or self.dirtyRev or null;
 
   environment.systemPackages = lib.attrsets.attrValues {
     inherit
@@ -30,15 +46,4 @@
       duti # file associations
       ;
   };
-
-  programs.man.enable = true;
-  programs.info.enable = true;
-
-  system.checks = {
-    verifyBuildUsers = true;
-    verifyNixPath = false;
-  };
-
-  system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null; # Set Git commit hash for darwin-version.
-  system.stateVersion = 4;
 }
