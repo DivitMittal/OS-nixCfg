7d7c35b72956ee3c673f5ff6a62e348da40baadf
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-25 19:42:36 +0530

    refactor: set classes at configuration-level flak output for mkHost calls further

--- hosts/default.nix
+++ hosts/default.nix
@@ -3,38 +3,44 @@
   mkHost,
   ...
 }: {
-  flake.darwinConfigurations = {
+  flake.darwinConfigurations = let
+    class = "darwin";
+  in {
     L1 = mkHost {
+      inherit class;
       hostName = "L1";
       system = "x86_64-darwin";
-      class = "darwin";
     };
   };
 
-  flake.nixosConfigurations = {
+  flake.nixosConfigurations = let
+    class = "nixos";
+  in {
     WSL = mkHost {
+      inherit class;
       hostName = "WSL";
       system = "x86_64-linux";
-      class = "nixos";
       additionalModules = [
         inputs.nixos-wsl.nixosModules.default
       ];
     };
     # L2 = mkHost {
+    #   inherit class;
     #   hostName = "L2";
     #   system = "x86_64-linux";
-    #   class = "nixos";
     #   additionalModules = [
     #     /etc/nixos/hardware-configuration.nix # impure
     #   ];
     # };
   };
 
-  flake.nixOnDroidConfigurations = {
+  flake.nixOnDroidConfigurations = let
+    class = "droid";
+  in {
     M1 = mkHost {
+      inherit class;
       hostName = "M1";
       system = "aarch64-linux";
-      class = "droid";
     };
   };
 }
