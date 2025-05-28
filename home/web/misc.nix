9fe983c813a304409e4451b5862671a4c80acc48
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-28 22:20:22 +0530

    feat(home): migrate to chawan tui web-browser

--- home/web/misc.nix
+++ home/web/misc.nix
@@ -1,12 +1,20 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
-  home.packages = lib.attrsets.attrValues {
-    inherit
-      (pkgs)
-      w3m
-      ;
+{pkgs, ...}: {
+  # home.packages = lib.attrsets.attrValues {
+  #   inherit
+  #     (pkgs)
+  #   #w3m
+  #   #browsh
+  #     ;
+  # };
+
+  programs.chawan = {
+    enable = true;
+    package = pkgs.chawan;
+    settings = {
+      buffer = {
+        images = true;
+        autofocus = true;
+      };
+    };
   };
 }
