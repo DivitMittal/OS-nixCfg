19b530c79affde1a15194fbbf8d912887316af83
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-06-05 22:27:27 +0530

    feat(home): add zathura pdf viewer

--- home/desktop-env/misc.nix
+++ home/desktop-env/misc.nix
@@ -5,9 +5,15 @@
   ...
 }: {
   home.packages = lib.attrsets.attrValues {
+    # Office files (e.g., .docx, .xlsx, .pptx)
     onlyoffice =
       if hostPlatform.isDarwin
       then (pkgs.brewCasks.onlyoffice.override {variation = "sequoia";})
       else pkgs.onlyoffice;
+  };
+
+  programs.zathura = {
+    enable = true;
+    package = pkgs.zathura;
   };
 }
