e572ec0f86015f3b3ea4d556ef5b2d769eea6ef5
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-06-06 14:27:02 +0530

    refactor(home): remove hammerspoon README markdown

--- home/gui/darwin/hammerspoon/default.nix
+++ home/gui/darwin/hammerspoon/default.nix
@@ -7,6 +7,7 @@
 in {
   home.packages = lib.lists.optionals enable [pkgs.brewCasks.hammerspoon];
 
+  # run once: defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
   xdg.configFile."hammerspoon/init.lua" = {
     inherit enable;
     source = ./init.lua;
