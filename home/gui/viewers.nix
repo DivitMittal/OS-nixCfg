b9c8056dfd6bf128adf79fdc17be1b8a93511c40
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-06-09 05:13:29 +0530

    feat(home): add zathura config

--- home/gui/viewers.nix
+++ home/gui/viewers.nix
@@ -16,5 +16,13 @@
   programs.zathura = {
     enable = true;
     package = pkgs.zathura;
+    options = {
+      adjust-open = "best-fit";
+      pages-per-row = 1;
+      scroll-page-aware = "true"; # mindful of the page's end
+      smooth-scroll = "true";
+      scroll-full-overlap = 0.01;
+      scroll-step = 100;
+    };
   };
 }
