2c286dc527b262902b3491427206f2015624d643
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-06-22 02:34:03 +0530

    feat(home): add au-lab brewCask

--- home/media/music/create.nix
+++ home/media/music/create.nix
@@ -5,17 +5,22 @@
   ...
 }: {
   home.packages = lib.attrsets.attrValues {
-    # inherit(pkgs)
-    #   reaper-sws-extension
-    #   reaper-reapack-extension
-    # ;
+    inherit
+      (pkgs)
+      # reaper-sws-extension
+      # reaper-reapack-extension
+      ;
+    inherit
+      (pkgs.brewCasks)
+      au-lab
+      ;
     # reaper =
     #   if hostPlatform.isDarwin
     #   then pkgs.brewCasks.reaper
     #   else pkgs.reaper;
     musescore =
       if hostPlatform.isDarwin
-        then pkgs.brewCasks.musescore
+      then pkgs.brewCasks.musescore
       else pkgs.musescore;
   };
 }
