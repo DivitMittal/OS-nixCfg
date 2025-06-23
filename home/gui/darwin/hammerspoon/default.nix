6d04db1c7b4a8eb2e27a9d7433746b5bfc717a1b
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-06-24 02:27:43 +0530

    feat(home): add new homeManagerModule for hammerspoon with support for spoons

--- home/gui/darwin/hammerspoon/default.nix
+++ home/gui/darwin/hammerspoon/default.nix
@@ -1,27 +1,29 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}: let
-  vimModeSpoon = pkgs.fetchFromGitHub {
-  owner = "dbalatero";
-  repo = "VimMode.spoon";
-  rev = "dda997f79e240a2aebf1929ef7213a1e9db08e97";
-  hash = "sha256-zpx2lh/QsmjP97CBsunYwJslFJOb0cr4ng8YemN5F0Y=";
-};
-in {
-  home.packages = lib.lists.optionals config.xdg.configFile."hammerspoon".enable [pkgs.brewCasks.hammerspoon];
+{pkgs, ...}: {
+  # home.packages = lib.lists.optionals config.xdg.configFile."hammerspoon".enable [pkgs.brewCasks.hammerspoon];
 
   # run once: defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
-  xdg.configFile."hammerspoon" = {
+  # xdg.configFile."hammerspoon" = {
+  #   enable = true;
+  #   source = ./config;
+  #   recursive = true;
+  # };
+  # xdg.configFile."hammerspoon/Spoons/VimMode.spoon" = {
+  #   enable = true;
+  #   source = vimModeSpoon;
+  #   recursive = true;
+  # };
+  programs.hammerspoon = {
     enable = true;
-    source = ./config;
-    recursive = true;
-  };
-  xdg.configFile."hammerspoon/Spoons/VimMode.spoon" = {
-    enable = true;
-    source = vimModeSpoon;
-    recursive = true;
+    package = pkgs.brewCasks.hammerspoon;
+
+    configPath = ./config;
+    spoons = {
+      VimMode = pkgs.fetchFromGitHub {
+        repo = "VimMode.spoon";
+        owner = "dbalatero";
+        rev = "dda997f79e240a2aebf1929ef7213a1e9db08e97";
+        hash = "sha256-zpx2lh/QsmjP97CBsunYwJslFJOb0cr4ng8YemN5F0Y=";
+      };
+    };
   };
 }
