4458f8668bb441a06901815da01ced798852a9be
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-06-26 05:17:13 +0530

    feat(home): add Reaper DAW

--- home/media/music/create.nix
+++ home/media/music/create.nix
@@ -4,29 +4,31 @@
   hostPlatform,
   ...
 }: {
-  home.packages = lib.attrsets.attrValues {
-    inherit
-      (pkgs)
-      # reaper-sws-extension
-      # reaper-reapack-extension
-      ;
-    au-lab =
-      if hostPlatform.isDarwin
-      then
-        (pkgs.brewCasks.au-lab.overrideAttrs (oldAttrs: {
-          src = pkgs.fetchurl {
-            url = lib.lists.head oldAttrs.src.urls;
-            hash = "sha256-sjlpgwwtaF4ldr8EI3Cpzboai+7eYj8LvrBjWf7chFk=";
-          };
-        }))
-      else null;
-    # reaper =
-    #   if hostPlatform.isDarwin
-    #   then pkgs.brewCasks.reaper
-    #   else pkgs.reaper;
-    musescore =
-      if hostPlatform.isDarwin
-      then pkgs.brewCasks.musescore
-      else pkgs.musescore;
-  };
+  home.packages = lib.attrsets.attrValues ({
+      # au-lab =
+      #   if hostPlatform.isDarwin
+      #   then
+      #     (pkgs.brewCasks.au-lab.overrideAttrs (oldAttrs: {
+      #       src = pkgs.fetchurl {
+      #         url = lib.lists.head oldAttrs.src.urls;
+      #         hash = "sha256-sjlpgwwtaF4ldr8EI3Cpzboai+7eYj8LvrBjWf7chFk=";
+      #       };
+      #     }))
+      #   else null;
+      reaper =
+        if hostPlatform.isDarwin
+        then pkgs.brewCasks.reaper
+        else pkgs.reaper;
+      musescore =
+        if hostPlatform.isDarwin
+        then pkgs.brewCasks.musescore
+        else pkgs.musescore;
+    }
+    // lib.attrsets.optionalAttrs (hostPlatform.isLinux) {
+      inherit
+        (pkgs)
+        reaper-sws-extension
+        reaper-reapack-extension
+        ;
+    });
 }
