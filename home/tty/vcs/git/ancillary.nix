ed190f6cc9b4a0affaa5273d197d111a716adc81
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-06-26 20:16:01 +0530

    feat: add geminicommit
    Adds geminicommit to the environment.

--- home/tty/vcs/git/ancillary.nix
+++ home/tty/vcs/git/ancillary.nix
@@ -8,6 +8,7 @@
     inherit
       (pkgs)
       transcrypt
+      geminicommit
       ;
     ghfetch = pkgs.writeShellScriptBin "ghfetch" ''
       ${pkgs.ghfetch}/bin/ghfetch --color magenta --access-token=$(cat ${config.age.secrets.github.path}) --user ${config.hostSpec.handle}
