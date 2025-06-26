8f73eeccf65be475b20c8948f4d89455e3b768ac
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-06-26 23:28:57 +0530

    refactor(home): move gcalcli and notes to productivity
    Moves gcalcli and notes to a new productivity module.
    This commit reorganizes the home configuration by grouping
    productivity-related tools into a dedicated module.

--- home/tools/productivity/misc.nix
+++ home/tools/productivity/misc.nix
@@ -6,12 +6,7 @@
   home.packages = lib.attrsets.attrValues {
     inherit
       (pkgs)
-      ttyper
+      gcalcli
       ;
-
-    # inherit
-    #   (pkgs.customDarwin)
-    #   cliclick-bin
-    #   ;
   };
 }
