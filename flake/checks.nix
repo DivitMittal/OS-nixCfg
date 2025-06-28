f06da58e2c1bb9a0d7b7ba03b041a879f47a1c23
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-06-29 01:13:12 +0530

    chore(templates): reformat pre-commit-hooks & sync with template
    Reformats the flakeModule pre-commit-hooks checks for better
    organization. Also syncs the checks with the vanilla template.

--- flake/checks.nix
+++ flake/checks.nix
@@ -9,7 +9,10 @@
       excludes = ["flake.lock"];
       default_stages = ["pre-commit"];
       hooks = {
+        ## Formatting
         treefmt.enable = false;
+        trim-trailing-whitespace.enable = true;
+        mixed-line-endings.enable = false;
 
         check-added-large-files = {
           enable = true;
@@ -24,12 +27,10 @@
         check-case-conflicts.enable = true;
         check-executables-have-shebangs.enable = true;
         check-shebang-scripts-are-executable.enable = true;
-        check-merge-conflicts.enable = true;
-        detect-private-keys.enable = true;
-        mixed-line-endings.enable = false;
 
         fix-byte-order-marker.enable = true;
-        trim-trailing-whitespace.enable = true;
+        check-merge-conflicts.enable = true;
+        detect-private-keys.enable = true;
       };
     };
   };
--- templates/vanilla/flake/checks.nix
+++ templates/vanilla/flake/checks.nix
@@ -9,7 +9,10 @@
       excludes = ["flake.lock"];
       default_stages = ["pre-commit"];
       hooks = {
+        ## Formatting
         treefmt.enable = false;
+        trim-trailing-whitespace.enable = true;
+        mixed-line-endings.enable = false;
 
         check-added-large-files = {
           enable = true;
@@ -22,11 +25,12 @@
           ];
         };
         check-case-conflicts.enable = true;
-        check-merge-conflicts.enable = true;
-        detect-private-keys.enable = false;
-        mixed-line-endings.enable = false;
+        check-executables-have-shebangs.enable = true;
+        check-shebang-scripts-are-executable.enable = true;
+
         fix-byte-order-marker.enable = true;
-        trim-trailing-whitespace.enable = true;
+        check-merge-conflicts.enable = true;
+        detect-private-keys.enable = true;
       };
     };
   };
