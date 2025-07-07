660ae920982ba4a9b805118cc7a5f43fae35ec69
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-07-07 06:58:56 +0530

    chore(home/web): update chrome cask hash
    The google-chrome cask's sha256 hash has been updated.

--- home/web/chromium.nix
+++ home/web/chromium.nix
@@ -7,7 +7,7 @@
   chrome = pkgs.brewCasks.google-chrome.overrideAttrs (oldAttrs: {
     src = pkgs.fetchurl {
       url = lib.lists.head oldAttrs.src.urls;
-      hash = "sha256-z4S/hzDPOTNtS3r7rSsACpW+pP3t8KEOVPODp7DcW2Q=";
+      hash = "sha256-R/k9gdOthgiME1/zK+6qQ6oqNgHme76iTkybVVFSU9I=";
     };
   });
 in {
@@ -27,4 +27,3 @@ in {
 # GPU rasterization
 # Parallel downloading
 # Experimental QUIC protocol
-
