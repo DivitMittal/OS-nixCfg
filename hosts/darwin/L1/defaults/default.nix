e587442d5522c542e2bb6e889a4ab51d13e46b5a
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-07-07 13:18:13 +0530

    feat(darwin): major defaults configuration update
    Declaratively configure macOS with extensive defaults settings.
    Includes UI, Finder, Dock, keyboard, mouse, and accessibility.

--- hosts/darwin/L1/defaults/default.nix
+++ hosts/darwin/L1/defaults/default.nix
@@ -1,24 +1,102 @@
-_: {
+{config, ...}: {
   security.pam.services.sudo_local.touchIdAuth = true;
 
   system.defaults = {
+    ".GlobalPreferences" = {
+      "com.apple.mouse.scaling" = 1.75;
+    };
+
+    NSGlobalDomain = {
+      AppleMetricUnits = 1;
+      AppleMeasurementUnits = "Centimeters";
+      AppleTemperatureUnit = "Celsius";
+      ## Finder
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+      NSDocumentSaveNewDocumentsToCloud = false;
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSTableViewDefaultSizeMode = 2; # 1 = Small, 2 = Medium, 3 = Large (sidebar icons)
+      "com.apple.springing.enabled" = false;
+      ## Windows
+      AppleWindowTabbingMode = "manual";
+      NSAutomaticWindowAnimationsEnabled = false;
+      NSWindowShouldDragOnGesture = true;
+      ## UI
+      _HIHideMenuBar = false;
+      AppleInterfaceStyle = "Dark";
+      AppleInterfaceStyleSwitchesAutomatically = false;
+      NSTextShowsControlCharacters = true;
+      PMPrintingExpandedStateForPrint = true;
+      AppleShowScrollBars = "WhenScrolling";
+      #AppleFontSmoothing = 2;
+      ## Keyboard
+      ApplePressAndHoldEnabled = false;
+      InitialKeyRepeat = 15; # First key repeat delay, default is 15 (225 ms); 1 = 15ms
+      KeyRepeat = 1; # Subsequent key repeat delay, default is 2 (30 ms); 1 = 15ms
+      AppleKeyboardUIMode = 2; # 0 = Disabled 2 = Enabled on Sonoma or later 3 = Enabled on older macOS versions
+      "com.apple.keyboard.fnState" = false; # media/control/function keys will default to function keys
+      ## Autocorrect
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticInlinePredictionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+      ## Mouse/Trackpad
+      "com.apple.mouse.tapBehavior" = 1; # tap to click
+      "com.apple.trackpad.enableSecondaryClick" = true;
+      "com.apple.trackpad.forceClick" = true;
+      "com.apple.swipescrolldirection" = true; # natural scrolling
+      "com.apple.trackpad.scaling" = 1.75;
+      "com.apple.trackpad.trackpadCornerClickBehavior" = null;
+      ## UX
+      "com.apple.sound.beep.feedback" = 1;
+      "com.apple.sound.beep.volume" = 0.3;
+    };
+
+    ## Stage Manager
+    WindowManager = {
+      GloballyEnabled = false;
+      AppWindowGroupingBehavior = true; # false means “One at a time” true means “All at once”
+      EnableStandardClickToShowDesktop = true;
+    };
+
+    ## Control Center
+    controlcenter = {
+      AirDrop = false;
+      Bluetooth = true;
+      NowPlaying = false;
+      Sound = true;
+    };
+
+    ## Dock
     dock = {
-      ### Dock
+      ## Autohide
       autohide = true;
+      autohide-delay = 0.175;
+      autohide-time-modifier = 1.5;
+      ## Mission Control
+      expose-animation-duration = 1.5;
+      expose-group-apps = false;
+      ## UI
+      mouse-over-hilite-stack = true;
       orientation = "right";
-      persistent-apps = [];
-      static-only = true;
-      show-recents = false;
       show-process-indicators = false;
       showhidden = true;
-      tilesize = 20;
-      magnification = true;
-      largesize = 64;
       minimize-to-application = false;
+      ## Animation
+      launchanim = false;
+      tilesize = 20;
+      largesize = 64;
+      magnification = true;
       mineffect = "genie";
-      ### Spaces
+      ## Items
+      persistent-apps = [];
+      static-only = true; # only open apps
+      show-recents = false;
+      ## Spaces
       mru-spaces = false; # most-recently used spaces ordering
-      ### Hot-Corners
+      ## Hot-Corners
       wvous-bl-corner = 1;
       wvous-br-corner = 1;
       wvous-tl-corner = 10;
@@ -26,95 +104,156 @@ _: {
     };
 
     finder = {
-      QuitMenuItem = true;
       AppleShowAllFiles = true;
       AppleShowAllExtensions = true;
+      QuitMenuItem = true;
       ShowPathbar = true;
+      NewWindowTarget = "Home";
       ShowStatusBar = false;
+      _FXSortFoldersFirst = true;
+      _FXSortFoldersFirstOnDesktop = true;
       _FXShowPosixPathInTitle = true;
+      FXRemoveOldTrashItems = true; # Remove items from the Trash after 30 days
       FXEnableExtensionChangeWarning = false;
-      FXDefaultSearchScope = "SCcf"; # SCcf - Scope current folder
-      FXPreferredViewStyle = "icnv"; # icnv - icnv
+      FXDefaultSearchScope = "SCcf"; # SCcf(SCope current folder)
+      FXPreferredViewStyle = "icnv";
+    };
+
+    ## HID
+    hitoolbox = {
+      AppleFnUsageType = "Do Nothing";
+    };
+
+    iCal = {
+      "first day of week" = "Monday";
     };
 
     loginwindow = {
       GuestEnabled = false;
-      DisableConsoleAccess = false;
+      DisableConsoleAccess = false; # typing “>console” for a username
+      PowerOffDisabledWhileLoggedIn = false;
+      RestartDisabled = false;
+      RestartDisabledWhileLoggedIn = false;
+      ShutDownDisabled = false;
+      ShutDownDisabledWhileLoggedIn = false;
+      SleepDisabled = false;
+      autoLoginUser = "${config.hostSpec.username}";
     };
 
-    NSGlobalDomain = {
-      AppleMetricUnits = 1;
-      AppleMeasurementUnits = "Centimeters";
-      AppleTemperatureUnit = "Celsius";
-      AppleWindowTabbingMode = "always";
-      ### UI
-      _HIHideMenuBar = false;
-      AppleInterfaceStyle = "Dark";
-      AppleInterfaceStyleSwitchesAutomatically = false;
-      AppleShowScrollBars = "WhenScrolling";
-      AppleFontSmoothing = 2;
-      ### Keyboard
-      ApplePressAndHoldEnabled = false;
-      InitialKeyRepeat = 14; # First key repeat delay, default is 15 (225 ms); 1           = 15ms
-      KeyRepeat = 1; # Subsequent key repeat delay, default is 2 (30 ms); 1 = 15ms
-      AppleKeyboardUIMode = 3;
-      NSTextShowsControlCharacters = true;
-      "com.apple.keyboard.fnState" = false; # media/control/function keys will default to function keys
-      NSAutomaticCapitalizationEnabled = false;
-      NSAutomaticDashSubstitutionEnabled = false;
-      NSAutomaticPeriodSubstitutionEnabled = false;
-      NSAutomaticQuoteSubstitutionEnabled = false;
-      NSAutomaticSpellingCorrectionEnabled = false;
-      ### Mouse/Trackpad
-      "com.apple.mouse.tapBehavior" = 1;
-      "com.apple.swipescrolldirection" = true; # natural scrolling
-      "com.apple.trackpad.scaling" = 1.75;
-      "com.apple.trackpad.enableSecondaryClick" = true;
-      "com.apple.trackpad.trackpadCornerClickBehavior" = null;
-      NSWindowShouldDragOnGesture = true;
-      ### UX
-      "com.apple.sound.beep.feedback" = 1;
-      "com.apple.sound.beep.volume" = 0.3;
+    menuExtraClock = {
+      FlashDateSeparators = false; # Do not flash the date separators for every second
+      IsAnalog = false; # Show clock as analog
+      Show24Hour = false;
+      ShowAMPM = true;
+      ShowDate = 1; # 0 = when space, 1 = always, 2 = never
+      ShowDayOfMonth = true;
+      ShowDayOfWeek = true;
+      ShowSeconds = false;
+    };
+
+    screencapture = {
+      location = "~/Pictures/Screenshots/";
+      type = "png"; # png, jpg, tiff, pdf, ps, eps
+      disable-shadow = true; # Disable shadow in screenshots
+      show-thumbnail = false; # Do not show a thumbnail of the screenshot in the corner of the screen
+    };
+
+    screensaver = {
+      askForPassword = true; # Require password after sleep or screen saver begins
+      askForPasswordDelay = 0; # Immediately require password
     };
 
     trackpad = {
-      Clicking = true;
-      TrackpadRightClick = true; # tap-to-click
-      FirstClickThreshold = 0;
-      SecondClickThreshold = 2; # force touch left click thresholds
+      ActuationStrength = 0; # Force Touch actuation strength
+      Clicking = true; # tap-to-click
+      TrackpadRightClick = true; # two-finger right click
+      FirstClickThreshold = 0; # force touch left click thresholds
+      SecondClickThreshold = 0; # force touch right click thresholds
       Dragging = false; # tap-to-drag
       TrackpadThreeFingerDrag = true;
+      TrackpadThreeFingerTapGesture = 2;
     };
 
     LaunchServices.LSQuarantine = false;
     SoftwareUpdate.AutomaticallyInstallMacOSUpdates = false;
   };
 
-  system.defaults.CustomUserPreferences = {
-    NSGlobalDomain = {
-      WebKitDeveloperExtras = true; # Add a context menu item for showing the Web Inspector in web views
-      AppleLocale = "en_US@currency=USD";
-      AppleLanguages = ["en"];
-    };
-
-    "com.apple.finder" = {
-      _FXSortFoldersFirst = true;
-      NewWindowTargetPath = "PfHm"; # Home - PfHm; Desktop - PfDe; Documents - PfDo
-    };
-
-    "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
+  system.defaults.CustomSystemPreferences = {
+    ## Image Capture
     "com.apple.ImageCapture".disableHotPlug = true; # Auto
-    "com.apple.commerce".AutoUpdate = true;
-    "com.apple.AdLib".allowApplePersonalizedAdvertising = false;
+    ## Ads
+    "com.apple.AdLib" = {
+      allowApplePersonalizedAdvertising = false;
+      StocksEnabled = false;
+      allowIdentifierForAdvertising = false;
+    };
+    ## Desktop Services
     "com.apple.desktopservices" = {
       # Avoid creating .DS_Store files on network or USB volumes
       DSDontWriteNetworkStores = true;
       DSDontWriteUSBStores = true;
     };
-    "com.apple.screensaver" = {
-      askForPassword = 1;
-      askForPasswordDelay = 0;
+    ## HID
+    "com.apple.HIToolbox" = {
+      AppleCurrentKeyboardLayoutInputSourceID = "com.apple.keylayout.ABC";
+      AppleDictationAutoEnable = 0;
     };
+    ## Text Edit
+    "com.apple.TextEdit".RichText = false;
+    ## Feedback Assistant
+    "com.apple.appleseed.FeedbackAssistant".Autogather = false;
+    ## TimeMachine
+    "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
+    ## App Store
+    "com.apple.commerce".AutoUpdate = false;
+  };
+
+  system.defaults.CustomUserPreferences = {
+    NSGlobalDomain = {
+      AppleLocale = "en_US@currency=USD";
+      AppleLanguages = ["en"];
+      AppleAccentColor = 0;
+      AppleHighlightColor = "1.000000 0.733333 0.721569 Red";
+      AppleMenuBarFontSize = "large";
+    };
+
+    ## Accessibility
+    "com.apple.universalaccess" = {
+      contrast = "0.04";
+      cursorIsCustomized = 1;
+      cursorFill = {
+        alpha = 1;
+        blue = 0;
+        green = 0;
+        red = 0;
+      };
+      cursorOutline = {
+        alpha = 1;
+        blue = 0;
+        green = 0.15;
+        red = 1;
+      };
+    };
+
+    ## Touchbar
+    "com.apple.controlstrip" = {
+      FullCustomized = [
+        "com.apple.system.sleep"
+        "com.apple.system.screen-saver"
+        "com.apple.system.screen-lock"
+        "NSTouchBarItemIdentifierFlexibleSpace"
+        "com.apple.system.group.keyboard-brightness"
+        "com.apple.system.group.media"
+      ];
+      MiniCustomized = [
+        "com.apple.system.brightness"
+        "com.apple.system.volume"
+        "com.apple.system.mute"
+      ];
+    };
+
+    ## Safari
+    NSGlobalDomain.WebKitDeveloperExtras = true; # Add a context menu item for showing the Web Inspector in web views
     "com.apple.Safari" = {
       UniversalSearchEnabled = false; # Privacy: don’t send search queries to Apple
       SuppressSearchSuggestions = true;
@@ -133,19 +272,14 @@ _: {
       WarnAboutFraudulentWebsites = true;
       WebKitJavaEnabled = false;
       WebKitJavaScriptCanOpenWindowsAutomatically = false;
-      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2TabsToLinks" = true;
-      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" = true;
-      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled" = false;
-      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled" = false;
-      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabledForLocalFiles" = false;
-      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically" = false;
+      "com.apple.Safari.ContentPageGroupIdentifier" = {
+        WebKit2TabsToLinks = true;
+        WebKit2DeveloperExtrasEnabled = true;
+        WebKit2BackspaceKeyNavigationEnabled = false;
+        WebKit2JavaEnabled = false;
+        WebKit2JavaEnabledForLocalFiles = false;
+        WebKit2JavaScriptCanOpenWindowsAutomatically = false;
+      };
     };
-    "com.apple.screencapture" = {
-      location = "~/Pictures/Screenshots/";
-      type = "png";
-    };
-    "com.apple.TextEdit".RichText = false;
-    "com.apple.HIToolbox".AppleFnUsageType = 0; # fn key does nothing
-    "com.apple.appleseed.FeedbackAssistant".Autogather = false;
   };
 }
