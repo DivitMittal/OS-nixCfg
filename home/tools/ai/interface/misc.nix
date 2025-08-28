50ab30e451140cf829d2aeab4fc0bef4122503a5
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-08-28 23:04:53 +0530

    feat(ai): add nix-ai-tools packages

--- home/tools/ai/interface/misc.nix
+++ home/tools/ai/interface/misc.nix
@@ -1,17 +1,25 @@
 {
   lib,
   pkgs,
+  inputs,
   ...
 }: {
   home.packages = lib.attrsets.attrValues {
     openhands = pkgs.writeShellScriptBin "openhands" ''
       exec ${pkgs.uv}/bin/uv tool run --python 3.12 --from openhands-ai openhands "$@"
     '';
-    gemini = pkgs.writeShellScriptBin "gemini" ''
-      exec ${pkgs.pnpm}/bin/pnpm dlx @google/gemini-cli@latest "$@"
-    '';
     aider = pkgs.writeShellScriptBin "aider" ''
       exec ${pkgs.uv}/bin/uv tool run --python python3.12 --with pip --from aider-chat@latest aider "$@"
     '';
+    inherit
+      (inputs.nix-ai-tools.packages.${pkgs.system})
+      #amp
+      #qwen-code
+      #catnip
+      cursor-agent
+      forge
+      goose-cli
+      groq-code-cli
+      ;
   };
 }
