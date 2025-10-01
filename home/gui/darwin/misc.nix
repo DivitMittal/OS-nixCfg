15582c2847719facfc49fb4802b9afb66503695a
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-10-01 22:48:58 +0530

    feat(misc): comment out thunderbird and LosslessSwitcher & uncomment claude-desktop

--- home/gui/darwin/misc.nix
+++ home/gui/darwin/misc.nix
@@ -13,23 +13,23 @@
       #aldente # Battery Management
       #licecap # Screen GIF Capture
       #keycastr # Keystroke visualizer
-      thunderbird # Email client
+      #thunderbird # Email client
       ;
     inherit
       (pkgs.customDarwin)
       hot-bin # CPU temperature monitor for menubar
       menubar-dock-bin # macOS dock in menubar
       MultiSoundChanger-bin # aggregate-output volume control in menubar
-      LosslessSwitcher-bin # Lossless audio toggle in menubar
+      #LosslessSwitcher-bin # Lossless audio toggle in menubar
       ;
 
-    # claude-desktop = pkgs.brewCasks.claude.overrideAttrs (oldAttrs: {
-    #   installPhase =
-    #     oldAttrs.installPhase
-    #     + ''
-    #       # Clean bin due to collision with claude-code
-    #       rm -rf $out/bin
-    #     '';
-    # });
+    claude-desktop = pkgs.brewCasks.claude.overrideAttrs (oldAttrs: {
+      installPhase =
+        oldAttrs.installPhase
+        + ''
+          # Clean bin due to collision with claude-code
+          rm -rf $out/bin
+        '';
+    });
   };
 }
