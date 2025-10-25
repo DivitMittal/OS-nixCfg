9886c74df59eb19e26aa50f2343efb4a5b68469d
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-10-25 13:06:05 +0530

    feat(music): librescore nix store clash

--- home/media/music/create.nix
+++ home/media/music/create.nix
@@ -5,7 +5,7 @@
   ...
 }: {
   home.packages = lib.attrsets.attrValues ({
-      librescore = pkgs.writeShellScriptBin "librescore" ''
+      librescore = pkgs.writeShellScriptBin "dl-librescore" ''
         exec ${pkgs.pnpm}/bin/pnpm dlx dl-librescore@latest "$@"
       '';
     }
