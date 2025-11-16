69c42e7d263ddb5f52c6bf6d2cc20b5064450249
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-11-16 21:00:39 +0530

    chore(chromium): update google chrome hash

--- home/web/chromium.nix
+++ home/web/chromium.nix
@@ -7,7 +7,7 @@
   chrome = pkgs.brewCasks.google-chrome.overrideAttrs (oldAttrs: {
     src = pkgs.fetchurl {
       url = lib.lists.head oldAttrs.src.urls;
-      hash = "sha256-v1MwK8oFbBk+PcnP5tkh0WgYmzKGYr2VkxD+otKN9xg=";
+      hash = "sha256-iWXiWFsETuZB5pDRH3bXHU9l6rxEB6owe0f73C22ofU=";
     };
   });
 in {
