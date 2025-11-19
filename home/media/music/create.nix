599b0967ec74d0e5137f744265e76e0d55b14b12
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-11-19 15:06:23 +0530

    refactor(music): cleanup no longer needed reaper-specific files

--- home/media/music/create.nix
+++ home/media/music/create.nix
@@ -1,19 +1,11 @@
 {
   pkgs,
   lib,
-  hostPlatform,
   ...
 }: {
-  home.packages = lib.attrsets.attrValues ({
-      librescore = pkgs.writeShellScriptBin "dl-librescore" ''
-        exec ${pkgs.pnpm}/bin/pnpm dlx dl-librescore@latest "$@"
-      '';
-    }
-    // lib.attrsets.optionalAttrs hostPlatform.isLinux {
-      # inherit
-      #   (pkgs)
-      #   reaper-sws-extension
-      #   reaper-reapack-extension
-      #   ;
-    });
+  home.packages = lib.attrsets.attrValues {
+    librescore = pkgs.writeShellScriptBin "dl-librescore" ''
+      exec ${pkgs.pnpm}/bin/pnpm dlx dl-librescore@latest "$@"
+    '';
+  };
 }
