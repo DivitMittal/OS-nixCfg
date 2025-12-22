e392088db64cbeb4e98f2c6ccf541ae05f817555
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-12-22 15:02:38 +0530

    refactor(gui): move spotify in gui

--- home/gui/music.nix
+++ home/gui/music.nix
@@ -23,5 +23,18 @@
       if hostPlatform.isDarwin
       then pkgs.brewCasks.musescore
       else pkgs.musescore;
+
+    spotify =
+      if hostPlatform.isDarwin
+      then
+        (pkgs.brewCasks.spotify.override {
+          variation = "tahoe";
+        }).overrideAttrs (oldAttrs: {
+          src = pkgs.fetchurl {
+            url = lib.lists.head oldAttrs.src.urls;
+            hash = "sha256-N2tQTS9vHp93cRI0c5riVZ/8FSaq3ovDqh5K9aU6jV0=";
+          };
+        })
+      else pkgs.spotify;
   };
 }
--- home/media/music/spotify/misc.nix
+++ home/media/music/spotify/misc.nix
@@ -1,7 +1,6 @@
 {
   lib,
   pkgs,
-  hostPlatform,
   ...
 }: {
   home.packages = lib.attrsets.attrValues {
@@ -9,18 +8,5 @@
       (pkgs)
       spotdl
       ;
-
-    spotify =
-      if hostPlatform.isDarwin
-      then
-        (pkgs.brewCasks.spotify.override {
-          variation = "tahoe";
-        }).overrideAttrs (oldAttrs: {
-          src = pkgs.fetchurl {
-            url = lib.lists.head oldAttrs.src.urls;
-            hash = "sha256-N2tQTS9vHp93cRI0c5riVZ/8FSaq3ovDqh5K9aU6jV0=";
-          };
-        })
-      else pkgs.spotify;
   };
 }
