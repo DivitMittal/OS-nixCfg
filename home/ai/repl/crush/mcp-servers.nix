4f16a8341213d7c0b5640cfa0f7313701ed658ed
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-01-02 18:05:57 +0530

    refactor(ai): modularize crush configuration

---
+++ home/ai/repl/crush/mcp-servers.nix
@@ -0,0 +1,49 @@
+{pkgs, ...}: let
+  pnpmCommand = "${pkgs.pnpm}/bin/pnpm";
+  # uvCommand = "${pkgs.uv}/bin/uvx";
+in {
+  programs.crush.settings.mcp = {
+    ## modelcontextprotocol
+    filesystem = {
+      type = "stdio";
+      command = pnpmCommand;
+      args = ["dlx" "@modelcontextprotocol/server-filesystem"];
+    };
+    sequential-thinking = {
+      type = "stdio";
+      command = pnpmCommand;
+      args = ["dlx" "@modelcontextprotocol/server-sequential-thinking"];
+    };
+    memory = {
+      type = "stdio";
+      command = pnpmCommand;
+      args = ["dlx" "@modelcontextprotocol/server-memory"];
+    };
+    ## Microsoft
+    # playwright = {
+    #   type = "stdio";
+    #   command = pnpmCommand;
+    #   args = ["dlx" "@playwright/mcp"];
+    # };
+    # markitdown = {
+    #   type = "stdio";
+    #   command = uvCommand;
+    #   args = ["markitdown-mcp"];
+    # };
+    ## third-party
+    deepwiki = {
+      type = "http";
+      url = "https://mcp.deepwiki.com/mcp";
+    };
+    octocode = {
+      type = "stdio";
+      command = pnpmCommand;
+      args = ["dlx" "octocode-mcp@latest"];
+    };
+    ddg = {
+      type = "stdio";
+      command = pnpmCommand;
+      args = ["dlx" "duckduckgo-mcp-server"];
+    };
+  };
+}
