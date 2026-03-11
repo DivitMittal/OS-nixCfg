32f5bac10eb38e3685bcd33480bfc7f53408d7b5
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-03-12 01:14:36 +0530

    feat(music): add reaper-sws & reapack extensions with home.file symlinks

--- home/gui/music.nix
+++ home/gui/music.nix
@@ -3,7 +3,29 @@
   lib,
   hostPlatform,
   ...
-}: {
+}: let
+  ## REAPER plugin directory (platform-specific)
+  pluginsDir =
+    if hostPlatform.isDarwin
+    then "Library/Application Support/REAPER/UserPlugins"
+    else ".config/REAPER/UserPlugins";
+
+  ## Arch suffix used in extension filenames; Darwin calls aarch64 "arm64"
+  swsArch =
+    if hostPlatform.isAarch64
+    then
+      (
+        if hostPlatform.isDarwin
+        then "arm64"
+        else "aarch64"
+      )
+    else "x86_64";
+
+  ext =
+    if hostPlatform.isDarwin
+    then "dylib"
+    else "so";
+in {
   home.packages = lib.attrsets.attrValues {
     # au-lab =
     #   if hostPlatform.isDarwin
@@ -15,11 +37,18 @@
     #       };
     #     }))
     #   else null;
+
+    ## Musescore Score Editor
     musescore =
       if hostPlatform.isDarwin
       then pkgs.brewCasks.musescore
       else pkgs.musescore;
+
+    ## Reaper DAW
     inherit (pkgs.custom) reaper-bin;
+
+    ## Reaper Extensions
+    inherit (pkgs) reaper-sws-extension reaper-reapack-extension;
 
     # spotify =
     #   if hostPlatform.isDarwin
@@ -33,5 +62,11 @@
     #       };
     #     })
     #   else pkgs.spotify;
+  };
+
+  home.file = {
+    "${pluginsDir}/reaper_sws-${swsArch}.${ext}".source = "${pkgs.reaper-sws-extension}/UserPlugins/reaper_sws-${swsArch}.${ext}";
+
+    "${pluginsDir}/reaper_reapack-${swsArch}.${ext}".source = "${pkgs.reaper-reapack-extension}/UserPlugins/reaper_reapack-${swsArch}.${ext}";
   };
 }
