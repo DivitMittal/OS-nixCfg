9ed60c4bc2bf3d336e4de16d344d8322dc721f84
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-03-24 22:53:52 +0530

    chore(hammerspoon): remove VimMode spoon

--- home/gui/darwin/hammerspoon.nix
+++ home/gui/darwin/hammerspoon.nix
@@ -12,13 +12,5 @@ in {
     package = pkgs.brewCasks.hammerspoon;
 
     configPath = hammerspoon-nix + "/myCfg";
-    spoons = {
-      VimMode = pkgs.fetchFromGitHub {
-        repo = "VimMode.spoon";
-        owner = "dbalatero";
-        rev = "dda997f79e240a2aebf1929ef7213a1e9db08e97";
-        hash = "sha256-zpx2lh/QsmjP97CBsunYwJslFJOb0cr4ng8YemN5F0Y=";
-      };
-    };
   };
 }
