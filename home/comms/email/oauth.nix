a9c75e9f2c7dce498f97ec253776208b3f2ea193
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-03-27 15:35:55 +0530

    fix(email/oauth): expose oauth2 script as a proper home package
    Replaces the disabled home.file symlink with a writeShellScriptBin wrapper,
    making the oauth2 script available in PATH as a first-class package.

--- home/comms/email/oauth.nix
+++ home/comms/email/oauth.nix
@@ -1,17 +1,19 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  home.file."${config.home.sessionVariables.BIN_HOME}/oauth2" = let
+{pkgs, ...}: {
+  home.packages = let
     mutt = pkgs.fetchFromGitHub {
       owner = "muttmua";
       repo = "mutt";
       rev = "master";
       hash = "sha256-GiN1WjHzTICFOFCEqb39MWGu3EfxE4uvgN7hpjdpfrI=";
     };
-  in {
-    enable = false;
-    source = mutt + "/contrib/mutt_oauth2.py";
-  };
+  in [
+    # To authorize a Microsoft account (one-time per account):
+    #   oauth2 --authorize --provider microsoft --client-id <CLIENT_ID> user@outlook.com
+    # Thunderbird's production client ID (tenant: common):
+    #   https://hg.mozilla.org/comm-central/file/tip/mailnews/base/src/OAuth2Providers.sys.mjs
+    #   microsoft365ProductionAppId = "9e5f94bc-e8a4-4e73-b8be-63364c29d753"
+    (pkgs.writeShellScriptBin "oauth2" ''
+      exec ${pkgs.python3}/bin/python3 ${mutt}/contrib/mutt_oauth2.py "$@"
+    '')
+  ];
 }
