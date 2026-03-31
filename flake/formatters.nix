eecf9d1c6a416bf092e827212d877fd98b6562bf
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-04-01 02:37:25 +0530

    fix(formatters): use pkgs.prettier over removed nodePackages.prettier

--- flake/formatters.nix
+++ flake/formatters.nix
@@ -1,26 +1,31 @@
 {inputs, ...}: {
   imports = [inputs.treefmt-nix.flakeModule];
 
-  perSystem.treefmt = {
-    projectRootFile = "flake.nix";
-    settings.global = {
-      excludes = [
-        ".github/*"
-      ];
-    };
+  perSystem = {pkgs, ...}: {
+    treefmt = {
+      projectRootFile = "flake.nix";
+      settings.global = {
+        excludes = [
+          ".github/*"
+        ];
+      };
 
-    flakeCheck = false; # handled via pre-commit
+      flakeCheck = false; # handled via pre-commit
 
-    programs = {
-      #typos.enable = true;
-      ## Nix
-      alejandra.enable = true;
-      deadnix.enable = true;
-      statix.enable = true;
-      ## js & markdown
-      prettier.enable = true;
-      ## Shell-scripts
-      shfmt.enable = true;
+      programs = {
+        #typos.enable = true;
+        ## Nix
+        alejandra.enable = true;
+        deadnix.enable = true;
+        statix.enable = true;
+        ## js & markdown
+        prettier = {
+          enable = true;
+          package = pkgs.prettier;
+        };
+        ## Shell-scripts
+        shfmt.enable = true;
+      };
     };
   };
 }
