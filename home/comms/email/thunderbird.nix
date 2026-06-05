b50fb28f3ff6390d96fb6ede9d290627a75e244f
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-06-05 14:22:10 +0530

    feat(thunderbird): add NUR extensions and move accountsOrder to secrets
    Wires send-later, provider-for-google-calendar, and markdown-here-revival
    extensions via NUR; moves accountsOrder into OS-nixCfg-secrets/tbAccounts
    to keep account identifiers out of the public repo.

--- home/comms/email/thunderbird.nix
+++ home/comms/email/thunderbird.nix
@@ -2,7 +2,18 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  tb = pkgs.nur.repos.rycee.thunderbird-addons;
+  buildXpi = pkgs.nur.repos.rycee.lib.mozilla.mkBuildMozillaXpiAddon {inherit (pkgs) stdenv fetchurl;};
+  markdown-here-revival = buildXpi {
+    pname = "markdown-here-revival";
+    version = "4.0.9.1";
+    addonId = "markdown-here-revival@xul.calypsoblue.org";
+    url = "https://addons.thunderbird.net/thunderbird/downloads/file/1044024/markdown_here_revival-4.0.9.1-tb.xpi?src=";
+    sha256 = "17iy2c81pccbzcz572f84w7rz54zqh8ikn8gj662h9n370kakyv6";
+    meta = {};
+  };
+in {
   imports = [
     inputs.OS-nixCfg-secrets.homeManagerConfigurations.tbAccounts
   ];
@@ -13,23 +24,15 @@
 
     profiles."main" = {
       isDefault = true;
-      accountsOrder = [
-        "1mitDivG"
-        "2divibG"
-        "3devDivG"
-        "4finDivZ"
-        "5forDivG"
-        "divQeztaG"
-        "dpsDivG"
-        "divShare"
-        "divA"
-        "divO"
-        "divMuj"
-        "vanG"
-        "6aSwiptG"
-        "7aCiazG"
-        "spG"
-        "lmG"
+
+      settings = {
+        "extensions.autoDisableScopes" = 0;
+      };
+
+      extensions = [
+        tb.send-later
+        tb.provider-for-google-calendar
+        markdown-here-revival
       ];
     };
   };
