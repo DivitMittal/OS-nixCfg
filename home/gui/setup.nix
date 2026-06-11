1733af257178b76e71c6e5c76190608240fdf6b9
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-06-12 02:38:38 +0530

    refactor(gui): split viewers.nix into ebooks and office modules

--- home/gui/setup.nix
+++ home/gui/setup.nix
@@ -9,10 +9,11 @@
     ++ [(inputs.import-tree ./music)]
     ++ [
       ./comms.nix
+      ./ebooks.nix
       ./misc.nix
       ./notes.nix
+      ./office.nix
       ./video.nix
-      ./viewers.nix
     ]
     ++ lib.lists.optionals hostPlatform.isDarwin [(inputs.import-tree ./darwin)]
     ++ lib.lists.optionals hostPlatform.isLinux [(inputs.import-tree ./linux)];
