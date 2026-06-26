8435fe848ca6d4008a214d81ad0524272adc9603
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-06-26 20:34:22 +0530

    fix(media): fix spotatui cargo lock and disable announcements

--- home/media/music/spotify/spotatui.nix
+++ home/media/music/spotify/spotatui.nix
@@ -1,14 +1,25 @@
 {
   inputs,
   hostPlatform,
+  pkgs,
   ...
 }: {
   programs.spotatui = {
     enable = true;
-    package = inputs.spotatui.packages.${hostPlatform.system}.default;
+    package = inputs.spotatui.packages.${hostPlatform.system}.default.overrideAttrs (old: {
+      cargoDeps = pkgs.rustPlatform.importCargoLock {
+        lockFile = "${old.src}/Cargo.lock";
+        outputHashes = {
+          "librespot-audio-0.8.0" = "sha256-WejAb0fSLxAGJw3in1kpL3fEvTToUhvYwIaXJxN8BV4=";
+        };
+      };
+    });
     settings = {
       enable_global_song_count = false;
-      behavior.enable_discord_rpc = false;
+      behavior = {
+        enable_discord_rpc = false;
+        enable_announcements = false;
+      };
     };
   };
 }
