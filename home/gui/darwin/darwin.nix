1c84f917b2b8bf4383563abc8be62495bce299b4
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-07-11 01:17:42 +0530

    feat(gui): enable mac-app-util linkApps on darwin
    copyApps (rsync) is disabled because it requires App Management
    permission and can't produce real trampolines. linkApps (symlink)
    gives mac-app-util a stable source dir in ~/Applications/Home
    Manager Apps to build trampolines from.

--- home/gui/darwin/darwin.nix
+++ home/gui/darwin/darwin.nix
@@ -1,5 +1,9 @@
 {inputs, ...}: {
   imports = [inputs.mac-app-util.homeManagerModules.default];
 
+  # copyApps rsync-copies app bundles (needs App Management permission); we use
+  # linkApps (symlink) instead so mac-app-util's sync-trampolines has a source
+  # dir (~/Applications/Home Manager Apps) to build real trampolines from.
   targets.darwin.copyApps.enable = false;
+  targets.darwin.linkApps.enable = true;
 }
